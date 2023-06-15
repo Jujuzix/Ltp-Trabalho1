4f62a92a7153ae99aa5e2245adf1c7fd66454784
Luiz F. Picolo <maronez.juliana@gmail.com>
2023-06-15 07:42:09 -0400

    Inserindo tratamento de erros ao projeto

--- script.js
+++ script.js
@@ -8,9 +8,18 @@ class Produto{
     
     }
 
-    mostrarProdutos(){
-        
+    montdeErro(){
+        try{return this.mostrarProdutos();}
+        catch(erro){
+          console.log(erro.stack);  
+        }
+    }
 
+    mostrarProdutos(){
+     if(this.nome !=""&&  this.imagemPrdt!=""&& this.Preco!=""){
+ 
+       
+     
         return `
         <div class= "Produtos">
            <h1> ${this.nome}</h1>
@@ -19,6 +28,10 @@ class Produto{
            <p>Preço: ${this.Preco}</p>
         </div>
         `
+     }
+     else{
+        throw new Error("Esta faltando atibultos")
+     }
         
         //this.nome + this.DataCadastro + this.Descrição + this.Preço
     
@@ -51,7 +64,7 @@ return `
 
 
 const produtodestaq = new produtoDestaque("Tenis Telas Coil Refletivo","(01-02 data de cadastro)"," Tesla Coil Inspirado nos tenis dos anos 90, onde o tenis largo era a grande referencia do verdadeiro tenis de skate, para usar com aquela calça larga, o Coil e todo revistido de carmuça de alta resistentencia evitando o desgate rapido tendo contato com a lixa do skate.","R$ 379,90","https://static.dafiti.com.br/p/TESLA-T%C3%AAnis-Tesla-Coil-Refletivo-7135-13945501-1-zoom.jpg")
-const produto = new Produto("Tênis Nike Dunk ","(01-02 data de cadastro)","Tenis nike dunk","R$ 899,99","https://imgnike-a.akamaihd.net/250x250/024351BP.jpg")
+const produto = new Produto(" ","(01-02 data de cadastro)","Tenis nike dunk","R$ 899,99","https://imgnike-a.akamaihd.net/250x250/024351BP.jpg")
 const produto2 = new Produto("Tênis Nike Dunk ","(01-02 data de cadastro)","Tenis ","R$ 999,99","https://imgnike-a.akamaihd.net/250x250/024333P1.jpg")
 const produto3 = new Produto("Tênis Air Jordan 4","(01-02 data de cadastro)","Tenis ","R$ 1.799,99","https://imgnike-a.akamaihd.net/250x250/0253927T.jpg")
 
@@ -69,7 +82,8 @@ const produto3 = new Produto("Tênis Air Jordan 4","(01-02 data de cadastro)","T
  
 
 
-
+ console.log(produtodestaq.montdeErro());
+ 
  
     
 
